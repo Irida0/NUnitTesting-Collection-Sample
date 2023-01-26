e17886871358a46be78d46da61c0c2c4fe3dcf93
Irida0 <theirka0@gmail.com>
2023-01-26 09:49:40 +0200

    Add project files.

---
+++ Collection.UnitTests/ColectionTests.cs
@@ -0,0 +1,12 @@
+namespace Collection.UnitTests
+{
+    public class ColectionTests
+    {
+       
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Collection/Program.cs
@@ -0,0 +1,31 @@
+﻿using Collections;
+
+namespace Collection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var collection = new Collection<int>();
+            Console.WriteLine("Current collection: " + collection);
+
+            Console.WriteLine("Collection count: " + collection.Count);
+            Console.WriteLine("Collection capacity: " + collection.Capacity);
+
+            collection.Add(5);
+            Console.WriteLine("Current collection: " + collection);
+
+            collection.AddRange(6, 7);
+            Console.WriteLine("Current range: " + collection);
+
+            collection.InsertAt(0, 4);
+            Console.WriteLine("Current collection: " + collection);
+
+            collection.Exchange(0, 3);
+            Console.WriteLine("Current collection: " + collection);
+
+            collection.Clear();
+            Console.WriteLine("Current collection: " + collection);
+        }
+    }
+}
